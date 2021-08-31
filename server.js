958f09f9bfb113e573826e3f33f6231384798f38
Luis Ramirez <sdaewa22@gmail.com>
2021-08-31 16:51:27 +0200

    server created

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage }).array("file");
+
+app.post("/upload", function (req, res) {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err);
+    } else if (err) {
+      return res.status(500).json(err);
+    }
+    return res.status(200).send(req.file);
+  });
+});
+
+app.listen(port, () => console.log("Server connected"));
