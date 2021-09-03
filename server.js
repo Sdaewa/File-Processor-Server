e20b0e0e8645fb923b7b704fcfb0401a61da24d7
Luis Ramirez <sdaewa22@gmail.com>
2021-09-03 15:00:48 +0200

    trying another API

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const fs = require("fs");
 const path = require("path");
 const multer = require("multer");
 const cors = require("cors");
-const { PDFNet } = require("@pdftron/pdfnet-node");
-// const mimeType = require("./modules/mimeType");
-require("dotenv").config({ path: "./.env" });
+const mime = require("mime-types");
+require("dotenv").config({ path: ".env" });
+
+const CloudmersiveConvertApiClient = require("cloudmersive-convert-api-client");
+const defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
+const Apikey = defaultClient.authentications["Apikey"];
+Apikey.apiKey = process.env.API_KEY;
+
+const apiInstance = new CloudmersiveConvertApiClient.ConvertDocumentApi();
 
 const app = express();
 const port = 8000;
@@ -19,64 +25,99 @@ const storage = multer.diskStorage({
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/pdf" ||
-    file.mimetype === "image/jpeg" ||
-    file.mimetype === "application/pdf"
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
+// const fileFilter = (req, file, cb) => {
+//   if (
+//     file.mimetype === "application/msword" ||
+//     file.mimetype === "application/vnd.ms-word.document.macroEnabled.12" ||
+//     file.mimetype ===
+//       "	application/vnd.openxmlformats-officedocument.wordprocessingml.template" ||
+//     file.mimetype ===
+//       "	application/vnd.openxmlformats-officedocument.wordprocessingml.document" ||
+//     file.mimetype === "application/pdf"
+//   ) {
+//     cb(null, true);
+//   } else {
+//     cb(null, false);
+//   }
+// };
 
-const upload = multer({ storage: storage, fileFilter: fileFilter }).single(
-  "file"
-);
+const upload = multer({ storage: storage }).single("file");
 
 app.use(cors());
 
+app.use("/files", express.static("files"));
+
 const filesPath = path.join(__dirname + "/files");
 
-app.get("/convert/:filename", (req, res) => {
-  const filename = req.params.filename;
-  let ext = path.parse(filename).ext;
+// app.get("/convert", (req, res) => {
+//   console.log(filename);
+//   //   let ext = path.parse(filename).ext;
 
-  const inputPath = path.resolve(__dirname, filesPath, filename);
-  const outputPath = path.resolve(__dirname, filesPath, `${filename}.pdf`);
+//   //   if (ext === ".pdf") {
+//   //     res.statusCode = 500;
+//   //     res.end(`File is already PDF.`);
+//   //   }
 
-  if (ext === ".pdf") {
-    res.statusCode = 500;
-    res.end(`File is already PDF.`);
-  }
+//   const convertToPdf = async () => {
+//     const pdfdoc = await PDFNet.PDFDoc.create();
+//     await pdfdoc.initSecurityHandler();
+//     await PDFNet.Convert.toPdf(pdfdoc, inputPath);
+//     pdfdoc.save(outputPath, PDFNet.SDFDoc.SaveOptions.e_linearized);
+//   };
 
-  const main = async () => {
-    const pdfdoc = await PDFNet.PDFDoc.create();
-    await pdfdoc.initSecurityHandler();
-    await PDFNet.Convert.toPdf(pdfdoc, inputPath);
-    pdfdoc.save(outputPath, PDFNet.SDFDoc.SaveOptions.e_linearized);
+//   PDFNet.runWithCleanup(convertToPdf, process.env.API_KEY)
+//     .then(() => {
+//       fs.readFile((e, data) => {
+//         if (e) {
+//           res.status(500);
+//           res.send(e);
+//         } else {
+//   res.setHeader({ "Content-Type": "application/pdf" });
+//   res.send({ data });
+//           PDFNet.shutdown();
+//         }
+//       });
+//     })
+//     .catch((e) => {
+//       res.status(500);
+//       res.send(e);
+//       console.log(e);
+//     });
+// });
+
+app.get("/convert", (req, res) => {
+  const { filename } = req.query;
+  console.log(filename);
+  const inputPath = path.resolve(__dirname, `./files/${filename}`);
+
+  console.log(inputPath);
+  const inputFile = inputPath; // File | Input file to perform the operation on.
+
+  const callback = (error, data, req, res) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log("API called successfully. Returned data: " + data);
+    }
   };
 
-  PDFNetEndpoint(main, outputPath, res);
+  apiInstance.convertDocumentAutodetectToPdf(inputFile, callback);
 });
 
-// app.post("/convert", function (req, res) {
-//   fs.access("./files", (error) => {
-//     if (error) {
-//       fs.mkdirSync("./files");
-//     }
-//     upload(req, res, function (err) {
-//       if (err instanceof multer.MulterError) {
-//         return res.status(500).json(err);
-//       } else if (err) {
-//         return res.status(500).json(err);
-//       }
-//       return res.status(200).send(req.file);
-//     });
-//   });
-// });
+app.post("/", (req, res) => {
+  fs.access("./files", (error) => {
+    if (error) {
+      fs.mkdirSync("./files");
+    }
+    upload(req, res, function (err) {
+      if (err instanceof multer.MulterError) {
+        return res.status(500).json(err);
+      } else if (err) {
+        return res.status(500).json(err);
+      }
+      return res.status(200).send(req.file);
+    });
+  });
+});
 
 app.listen(port, () => console.log("Server connected"));
