8085ec8184840b5dfef785d05cfd7b9d180ef46b
Luis Ramirez <sdaewa22@gmail.com>
2021-10-18 23:19:35 +0200

    CORS error

--- server.js
+++ server.js
@@ -47,25 +47,25 @@ app.use(
   }).single("file")
 );
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested, Content-Type, Accept Authorization"
-//   );
-//   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-//     return res.status(200).json({});
-//   }
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+});
 
 app.use(uploadRoutes);
 app.use(downloadRoutes);
