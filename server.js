7977fafbfe98dd561dca4f8215894226ac668fea
Luis Ramirez <sdaewa22@gmail.com>
2021-10-18 11:33:24 +0200

    CORS error when in Heroku

--- server.js
+++ server.js
@@ -49,6 +49,16 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(deleteRoutes);
 app.use(uploadRoutes);
 app.use(downloadRoutes);
