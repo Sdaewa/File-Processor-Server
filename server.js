0d181b4130b13e026043e3b8f09a78f5997bcbbf
Luis Ramirez <sdaewa22@gmail.com>
2021-10-18 23:21:14 +0200

    CORS error

--- server.js
+++ server.js
@@ -57,10 +57,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
+  res.header("Access-Control-Allow-Headers");
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
     return res.status(200).json({});
