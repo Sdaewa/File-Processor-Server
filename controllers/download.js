679edeb429d1357cee2780142bb216f2a48ce23b
Luis Ramirez <sdaewa22@gmail.com>
2021-11-04 14:55:58 +0100

    data converted tp pdf bug fixed

--- controllers/download.js
+++ controllers/download.js
@@ -1,27 +1,41 @@
 const axios = require("axios");
-const cloudinary = require("cloudinary").v2;
+const { cloudinary } = require("../utils/cloudinary");
 
 require("dotenv").config({ path: ".env" });
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-});
-
 exports.downloadPdf = (req, res) => {
-  cloudinary.api.resources(function (err, res) {
-    if (err) {
-      return console.log(err);
-    }
-    const url = res.resources[0].url;
-    axios
-      .get(url, { responseType: "arraybuffer" })
-      .then((res) => {
-        res.send(res.data);
-      })
-      .catch((err) => {
+  cloudinary.api
+    .resources()
+    .then((result) => {
+      console.log(result.resources[0].url);
+      res.status(200).send({ url: result.resources[0].url });
+    })
+
+    .catch(
+      (err) => {
+        console.log(err);
         res.send(err);
-      });
-  });
+      }
+      // cloudinary.api.resources(function (err, res) {
+      //   if (err) {
+      //     return console.log(err);
+      //   }
+
+      //   const url = res.resources[0].url;
+
+      //   axios
+      //     .get(url, { responseType: "arraybuffer" })
+      //     .then((res) => {
+      //       return response.status(200).send({
+      //         message: "success",
+      //         response: res.data,
+      //       });
+      //     })
+      //     .catch((err) => {
+      //       return err;
+      //       // console.log(err);
+      //     });
+      // });
+      // res.status(200).send();
+    );
 };
--- controllers/upload.js
+++ controllers/upload.js
@@ -1,24 +1,35 @@
 const libre = require("libreoffice-convert");
 const { cloudinary } = require("../utils/cloudinary");
+const streamifier = require("streamifier");
+const fs = require("fs");
 
 require("dotenv").config({ path: ".env" });
 
 exports.upload = (req, res) => {
-  if (req.body.data !== undefined) {
-    const file = req.body.data;
+  console.log(req.file);
 
-    const extend = ".pdf";
+  if (req.file !== undefined) {
+    // const file = fs.readFileSync(req.body.data, { encoding: "base64" });
+    const file = req.file.buffer;
+    console.log("file=>", file);
+    // const docFile = fs.writeFileSync("file.doc", file);
+    // console.log("docFile=>", docFile);
+
     // Convert it to pdf format with undefined filter (see Libreoffice doc about filter)
+    const extend = ".pdf";
     libre.convert(file, extend, undefined, (err, data) => {
       if (err) {
         console.log(`Error converting file: ${err}`);
-        throw new Error({ error: error });
       }
-      const buffer = Buffer.from(data);
-      const fileString = buffer.toString("base64");
-      const docData = `data:image/jpeg;base64,${fileString}`;
+      console.log("data=>", data);
+      // const buffer = data;
+      // const fileString = buffer.toString("base64");
+      const pdfData = `data:application/pdf;base64,${data}`;
+      console.log(pdfData);
       cloudinary.uploader
-        .upload(docData)
+        .upload(pdfData, {
+          folder: "processor",
+        })
         .then((res) => {
           return response.status(200).send({
             message: "success",
@@ -31,7 +42,7 @@ exports.upload = (req, res) => {
             error,
           });
         });
-      res.status(200).send();
+      res.status(200).send(data);
     });
   } else {
     res.status(500).send();
