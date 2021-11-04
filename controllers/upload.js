19d91a57e2de18f2e5478d169f913b0973f31ca3
Luis Ramirez <sdaewa22@gmail.com>
2021-11-04 14:59:19 +0100

    converted and saved to folder cloudinary

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,19 +1,11 @@
 const libre = require("libreoffice-convert");
 const { cloudinary } = require("../utils/cloudinary");
-const streamifier = require("streamifier");
-const fs = require("fs");
 
 require("dotenv").config({ path: ".env" });
 
 exports.upload = (req, res) => {
-  console.log(req.file);
-
   if (req.file !== undefined) {
-    // const file = fs.readFileSync(req.body.data, { encoding: "base64" });
     const file = req.file.buffer;
-    console.log("file=>", file);
-    // const docFile = fs.writeFileSync("file.doc", file);
-    // console.log("docFile=>", docFile);
 
     // Convert it to pdf format with undefined filter (see Libreoffice doc about filter)
     const extend = ".pdf";
@@ -21,10 +13,9 @@ exports.upload = (req, res) => {
       if (err) {
         console.log(`Error converting file: ${err}`);
       }
-      console.log("data=>", data);
-      // const buffer = data;
-      // const fileString = buffer.toString("base64");
-      const pdfData = `data:application/pdf;base64,${data}`;
+
+      const fileString = data.toString("base64");
+      const pdfData = `data:application/pdf;base64,${fileString}`;
       console.log(pdfData);
       cloudinary.uploader
         .upload(pdfData, {
