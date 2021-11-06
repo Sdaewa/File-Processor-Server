0110fee8f6584df819f7d8006a3dd46ce39e73e1
Luis Ramirez <sdaewa22@gmail.com>
2021-11-06 13:00:03 +0100

    back to pdfco

--- controllers/minPdf.js
+++ controllers/minPdf.js
@@ -9,12 +9,43 @@ exports.convertToMin = (req, res) => {
   cloudinary.api
     .resources()
     .then((result) => {
-      const file = cloudinary.image(result.resources[0].public_id, {
-        quality: 50,
+      const url = result.resources[0].url;
+
+      var request = require("request").defaults({
+        encoding: null,
+      });
+      request.get(url, function (err, res, body) {
+        let reqOptions = {
+          uri: process.env.PDF_CO_URL,
+          headers: { "x-api-key": process.env.API_KEY },
+          formData: {
+            name: "minPDf/pdf",
+            file: body,
+          },
+        };
+        // Send request
+        request.post(reqOptions, function (error, res, body) {
+          // Parse JSON response
+          if (!error) {
+            console.log(error);
+
+            // // Download PDF file
+
+            axios.get(body, (response2) => {
+              console.log(response2);
+              // response2.pipe(file).on("close", () => {
+              // console.log(`Generated PDF file saved as "${pathToMin}" file.`);
+              // });
+            });
+          } else {
+            // Service reported error
+            console.log("Error: " + data.message);
+            // throw new Error({ error: data.message });
+          }
+        });
       });
 
-      console.log(file);
-      res.status(200).send(file);
+      res.status(200).send({ url: result.resources[0].url });
     })
     .catch((err) => {
       console.log(err);
