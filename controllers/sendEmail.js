7caacddc5d9732813b726e4c71cce0d7efd07f39
Luis Ramirez <sdaewa22@gmail.com>
2021-11-10 17:36:35 +0100

    email from cloud

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,9 +1,12 @@
 const MailGen = require("mailgen");
+const request = require("request").defaults({ encoding: null });
 const sg = require("@sendgrid/mail");
 
 require("dotenv").config({ path: ".env" });
 
 const { cloudinary } = require("../utils/cloudinary");
+
+// console.log(fileData);
 
 sg.setApiKey(process.env.SG_KEY);
 
@@ -22,38 +25,50 @@ exports.sendByEmail = (req, res) => {
       intro: "Welcome, find attached your PDF file",
     },
   };
+
   const emailTemplate = mailGenerator.generate(email);
-  require("fs").writeFileSync("preview.html", emailTemplate, "utf8");
-  const fileName = fs.readdirSync(path.resolve(__dirname, "../files/pdf"));
-  const pathToPdf = path.join(__dirname, `../files/pdf/${fileName[0]}`);
-  attachment = fs.readFileSync(pathToPdf);
+  // require("fs").writeFileSync("preview.html", emailTemplate, "utf8");
+  // const fileName = fs.readdirSync(path.resolve(__dirname, "../files/pdf"));
+  // const pathToPdf = path.join(__dirname, `../files/pdf/${fileName[0]}`);
+  // attachment = fs.readFileSync(pathToPdf);
   const { emailAddress } = req.body;
 
-  const msg = {
-    to: emailAddress,
-    from: process.env.EMAIL,
-    subject: "This is your PDF file 📄",
-    text: "and easy to do anywhere, even with Node.js",
-    html: emailTemplate,
-    attachments: [
-      {
-        content: attachment.toString("base64"),
-        filename: `${fileName}`,
-        type: "application/pdf",
-        disposition: "attachment",
-        content_id: "mytext",
-      },
-    ],
-  };
-  sg.send(msg)
-    .then((response) => {
-      fs.unlinkSync(pathToPdf);
-      res.sendStatus(response[0].statusCode);
+  cloudinary.api
+    .resources()
+    .then((result) => {
+      const url = result.resources[0].url;
+      request.get(url, function (err, res, body) {
+        const msg = {
+          to: emailAddress,
+          from: process.env.EMAIL,
+          subject: "This is your PDF file 📄",
+          text: "and easy to do anywhere, even with Node.js",
+          html: emailTemplate,
+          attachments: [
+            {
+              content: body.toString("base64"),
+              filename: "file.pdf",
+              type: "application/pdf",
+              disposition: "attachment",
+              content_id: "mytext",
+            },
+          ],
+        };
+        sg.send(msg)
+          .then((response) => {
+            res.sendStatus(response[0].statusCode);
+          })
+          .catch((error) => {
+            /* log friendly error */
+            console.error(error.toString());
+            // throw new Error({ error: error });
+          });
+      });
     })
-    .catch((error) => {
-      /* log friendly error */
-      console.error(error.toString());
-      // throw new Error({ error: error });
+    .catch((err) => {
+      console.log(err);
+      res.send(err);
     });
+
   // });
 };
