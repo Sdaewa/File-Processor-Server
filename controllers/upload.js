c42d972b6709cda6fdd994485e2e74640b36ce59
Luis Ramirez <sdaewa22@gmail.com>
2021-11-17 15:45:29 +0100

    aws setup

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,33 +1,36 @@
 const convertapi = require("convertapi")(process.env.CONVERT_API);
 const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
 const { cloudinary } = require("../utils/cloudinary");
 
 require("dotenv").config({ path: ".env" });
 
 const S3 = new AWS.S3({
-  credentials: process.env.AWS_ACCESS_KEY_ID,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
 exports.upload = (req, res) => {
   if (req.file !== undefined) {
     const file = req.file.buffer;
-    const fileName = req.file.originalname;
-    const fileString = file.toString("base64");
-    const docData = `data:application/msword;base64,${fileString}`;
+    const fileName = req.file.originalname.split(".");
+    const fileExt = fileName[fileName.length - 1];
 
-    cloudinary.uploader
-      .upload(docData, {
-        public_id: fileName,
-        resource_type: "raw",
-        // raw_convert: "aspose",
-      })
-      .then((res) => {
-        // cloudinary.url(res.public_id + ".pdf");
-        // console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    const params = {
+      Bucket: process.env.AWS_BUCKET,
+      Key: `${uuidv4()}.${fileExt}`,
+      Body: file,
+    };
+
+    S3.upload(params, (err, data) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+      res.status(200).send(data);
+      console.log(data);
+    });
 
     res.status(200).send();
   } else {
